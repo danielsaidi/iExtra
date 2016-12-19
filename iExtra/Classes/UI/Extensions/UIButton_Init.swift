382ddc4266eea02cb9a805bd0ac2e945a05955ac
Daniel Saidi <daniel.saidi@gmail.com>
2016-12-19 22:33:13 +0100

    Add button extension

---
+++ iExtra/Classes/UI/Extensions/UIButton_Init.swift
@@ -0,0 +1,21 @@
+//
+//  UIButton_Init.swift
+//  iExtra
+//
+//  Created by Daniel Saidi on 2016-12-19.
+//  Copyright © 2016 Daniel Saidi. All rights reserved.
+//
+
+import UIKit
+
+public extension UIButton {
+    
+    public convenience init(image: UIImage) {
+        self.init(type: .custom)
+        let size = image.size
+        frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        setImage(image, for: .normal)
+        setImage(image, for: .highlighted)
+        setImage(image, for: .selected)
+    }
+}
