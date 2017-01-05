1d7d035fc1f647245d3cdbe295ed19c3937808e2
Daniel Saidi <daniel.saidi@gmail.com>
2017-01-05 17:30:21 +0100

    Add color extension

---
+++ iExtra/Classes/UI/Extensions/UIImage/UIImage_Color.swift
@@ -0,0 +1,23 @@
+//
+//  UIImage_Color.swift
+//  iExtra
+//
+//  Created by Daniel Saidi on 2017-01-05.
+//  Copyright © 2017 Daniel Saidi. All rights reserved.
+//
+
+import UIKit
+
+public extension UIImage {
+    
+    public convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
+        let rect = CGRect(origin: .zero, size: size)
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        color.setFill()
+        UIRectFill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        guard let cgImage = image?.cgImage else { return nil }
+        self.init(cgImage: cgImage)
+    }
+}
