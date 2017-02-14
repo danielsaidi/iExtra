210b7c4513747b9261c8fd17b80eec89fb9661df
Daniel Saidi <daniel.saidi@gmail.com>
2017-02-14 09:00:52 +0100

    Add search bar style extensions

---
+++ iExtra/Classes/UI/Extensions/UISearchBar_Style.swift
@@ -0,0 +1,16 @@
+//
+//  UISearchBar_Style.swift
+//  iExtra
+//
+//  Created by Daniel Saidi on 2017-02-13.
+//  Copyright © 2016 Daniel Saidi. All rights reserved.
+//
+
+import UIKit
+
+public extension UISearchBar {
+    
+    public func setTransparentBackground() {
+        setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
+    }
+}
