832808f3574b4e93206a47865ba5b75618387898
Daniel Saidi <daniel.saidi@gmail.com>
2017-10-08 22:11:35 +0200

    Make extensions return gesture

--- iExtra/Classes/UI/Extensions/UIView/UIView+Gestures.swift
+++ iExtra/Classes/UI/Extensions/UIView/UIView+Gestures.swift
@@ -9,13 +9,17 @@ import UIKit
 
 public extension UIView {
     
-    public func addLongPressAction(_ action: Selector, target: Any) {
+    @discardableResult
+    public func addLongPressAction(_ action: Selector, target: Any) -> UILongPressGestureRecognizer {
         let gesture = UILongPressGestureRecognizer(target: target, action: action)
         addGestureRecognizer(gesture)
+        return gesture
     }
     
-    public func addTapAction(_ action: Selector, target: Any) {
+    @discardableResult
+    public func addTapAction(_ action: Selector, target: Any) -> UITapGestureRecognizer {
         let gesture = UITapGestureRecognizer(target: target, action: action)
         addGestureRecognizer(gesture)
+        return gesture
     }
 }
