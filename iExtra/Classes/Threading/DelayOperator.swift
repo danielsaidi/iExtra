a18c19212c4f6be162d69bc4a1347b57b06f99a4
Daniel Saidi <daniel.saidi@gmail.com>
2017-12-04 20:44:36 +0100

    Refactor Threading classes

--- iExtra/Classes/Threading/DelayOperator.swift
+++ iExtra/Classes/Threading/DelayOperator.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public func delay(_ delay:Double, _ function:@escaping ()->()) {
+public func delay(_ delay: Double, _ function: @escaping ()->()) {
     DispatchQueue.main.asyncAfter(
         deadline: DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC),
         execute: function)
