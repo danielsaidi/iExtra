c013d6fba967ccfee90ceb2f104d73ff3200e8ca
Daniel Saidi <daniel.saidi@gmail.com>
2018-05-09 15:07:06 +0200

    Add theme classes

---
+++ iExtra/UI/Themes/Theme.swift
@@ -0,0 +1,14 @@
+//
+//  Theme.swift
+//  iExtra
+//
+//  Created by Daniel Saidi on 2017-09-07.
+//  Copyright © 2018 Daniel Saidi. All rights reserved.
+//
+
+import Foundation
+
+public protocol Theme {
+    
+    func applyTheme()
+}
---
+++ iExtra/UI/Themes/ThemedButton.swift
@@ -0,0 +1,86 @@
+//
+//  ThemedButton.swift
+//  iExtra
+//
+//  Created by Daniel Saidi on 2018-05-09.
+//  Copyright © 2018 Daniel Saidi. All rights reserved.
+//
+
+/*
+ 
+ This class can be used to easily apply custom button themes.
+ 
+ */
+
+import UIKit
+import Foundation
+
+open class ThemedButton: UIButton {
+    
+    
+    // MARK: - Initialization
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    open func setup() { }
+    
+    
+    // MARK: - Appearance
+    
+    @objc open dynamic var disabledBackgroundColor: UIColor?
+    
+    @objc open dynamic var highlightedBackgroundColor: UIColor?
+    
+    @objc open dynamic var normalBackgroundColor: UIColor? {
+        didSet { backgroundColor = normalBackgroundColor }
+    }
+    
+    @objc open dynamic var themedFont: UIFont? {
+        get { return titleLabel?.font }
+        set { titleLabel?.font = newValue }
+    }
+    
+    
+    // MARK: - Properties
+    
+    open var backgroundColorForCurrentState: UIColor? {
+        if isDisabled, let color = disabledBackgroundColor { return color }
+        if isHighlighted, let color = highlightedBackgroundColor { return color }
+        return normalBackgroundColor ?? backgroundColor
+    }
+    
+    open override var isEnabled: Bool {
+        didSet { updateAppearance() }
+    }
+    
+    open var isDisabled: Bool {
+        return !isEnabled || isSoftDisabled
+    }
+    
+    open override var isHighlighted: Bool {
+        didSet { updateAppearance() }
+    }
+    
+    open var isSoftDisabled: Bool = false {
+        didSet { updateAppearance() }
+    }
+    
+    fileprivate var allStates: [UIControlState] {
+        return [.normal, .highlighted, .disabled]
+    }
+    
+    
+    // MARK: - Public Functions
+    
+    open func updateAppearance() {
+        backgroundColor = backgroundColorForCurrentState
+    }
+}
