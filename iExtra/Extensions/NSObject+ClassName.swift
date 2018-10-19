404256c371587268b43b80fe71095edc6818e430
Daniel Saidi <daniel.saidi@gmail.com>
2018-10-19 16:19:05 +0200

    Add classname extension property

--- iExtra/Extensions/NSObject+ClassName.swift
+++ iExtra/Extensions/NSObject+ClassName.swift
@@ -13,4 +13,8 @@ public extension NSObject {
     public static var className: String {
         return String(describing: self)
     }
+    
+    public var className: String {
+        return type(of: self).className
+    }
 }
--- iExtraTests/Extensions/NSObject+ClassNameTests.swift
+++ iExtraTests/Extensions/NSObject+ClassNameTests.swift
@@ -10,34 +10,30 @@ import Quick
 import Nimble
 import iExtra
 
-class NSObject_ClassNameTests_Base: NSObject {}
-class NSObject_ClassNameTests_Sub: NSObject_ClassNameTests_Base {}
-
 class NSObject_ClassNameTests: QuickSpec {
     
     override func spec() {
         
-        describe("object class name") {
+        describe("class name") {
             
-            it("handles foundation classes") {
-                expect(NSObject.className).to(equal("NSObject"))
+            it("is valid for base class") {
+                expect(TestParent.className).to(equal("TestParent"))
             }
             
-            it("handles ui classes") {
-                expect(UIView.className).to(equal("UIView"))
-                expect(UITableView.className).to(equal("UITableView"))
-                expect(UITableViewCell.className).to(equal("UITableViewCell"))
+            it("is valid for subclass") {
+                expect(TestChild.className).to(equal("TestChild"))
             }
             
-            it("handles base view") {
-                let name = NSObject_ClassNameTests_Base.className
-                expect(name).to(equal("NSObject_ClassNameTests_Base"))
+            it("is valid for base class instance") {
+                expect(TestParent().className).to(equal("TestParent"))
             }
             
-            it("handles sub view") {
-                let name = NSObject_ClassNameTests_Sub.className
-                expect(name).to(equal("NSObject_ClassNameTests_Sub"))
+            it("is valid for subclass instance") {
+                expect(TestChild().className).to(equal("TestChild"))
             }
         }
     }
 }
+
+private class TestParent: NSObject {}
+private class TestChild: NSObject {}
