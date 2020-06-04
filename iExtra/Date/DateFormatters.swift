01ca77c6ac721841bc9f4620ad66a3701e62af95
Daniel Saidi <daniel.saidi@gmail.com>
2020-06-04 21:53:38 +0200

    Move date utils to SwiftKit

--- iExtra/Date/DateFormatters.swift
+++ iExtra/Date/DateFormatters.swift
@@ -10,24 +10,23 @@ import Foundation
 
 public extension DateFormatter {
     
-    static func formatter(
-        withFormat dateFormat: String,
+    convenience init(
+        dateFormat: String,
         calendar: Calendar = Calendar(identifier: .iso8601),
         locale: Locale = Locale(identifier: "en_US_POSIX"),
-        timeZone: TimeZone? = TimeZone(secondsFromGMT: 0)) -> DateFormatter {
-        let dateFormatter = DateFormatter()
-        dateFormatter.calendar = calendar
-        dateFormatter.locale = locale
-        dateFormatter.dateFormat = dateFormat
-        dateFormatter.timeZone = timeZone
-        return dateFormatter
+        timeZone: TimeZone? = TimeZone(secondsFromGMT: 0)) {
+        self.init()
+        self.calendar = calendar
+        self.locale = locale
+        self.dateFormat = dateFormat
+        self.timeZone = timeZone
     }
     
-    static var iso8601SecondFormatter: DateFormatter {
-        return formatter(withFormat: "yyyy-MM-dd'T'HH:mm:ssZ")
+    static var iso8601Second: DateFormatter {
+        DateFormatter(dateFormat: "yyyy-MM-dd'T'HH:mm:ssZ")
     }
     
-    static var iso8601MillisecondFormatter: DateFormatter {
-        return formatter(withFormat: "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+    static var iso8601Millisecond: DateFormatter {
+        DateFormatter(dateFormat: "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
     }
 }
